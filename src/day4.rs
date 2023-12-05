01ea330808447aac92b125be642e6a14ebf0e0a1
Vladimir Vukicevic <vladv@unity3d.com>
2023-12-04 18:43:42 -0800

    doh no loop needed

--- src/day4.rs
+++ src/day4.rs
@@ -2,7 +2,8 @@
 use std::{cmp::{min,max}};
 
 fn main() {
-    let data = std::fs::read_to_string("inputs/day4.txt").unwrap();
+    //let data = std::fs::read_to_string("inputs/day4.txt").unwrap();
+    let data = include_str!("../inputs/day4.txt");
 
     let mut sum: i64 = 0;
 
@@ -11,31 +12,29 @@ fn main() {
 
     for (i, line) in data.lines().enumerate() {
         let count = card_counts[i];
-        for _ in 0..count {
-            let rest = &line[line.find(":").unwrap()+2..];
-            let both = rest.split("|").map(|p| {
-                    p.trim().split_whitespace()
-                        .map(|s| s.parse::<i32>().unwrap())
-                        .collect::<Vec<i32>>()
-                    })
-                .collect::<Vec<_>>();
-            let card = &both[0];
-            let winning = &both[1];
+        let rest = &line[line.find(":").unwrap()+2..];
+        let both = rest.split("|").map(|p| {
+                p.trim().split_whitespace()
+                    .map(|s| s.parse::<i32>().unwrap())
+                    .collect::<Vec<i32>>()
+                })
+            .collect::<Vec<_>>();
+        let card = &both[0];
+        let winning = &both[1];
 
-            let mut num = 0;
-            for i in 0..card.len() {
-                if winning.contains(&card[i]) {
-                    num += 1;
-                }
+        let mut num = 0;
+        for i in 0..card.len() {
+            if winning.contains(&card[i]) {
+                num += 1;
             }
+        }
 
-            //println!("Card wins: {}", num);
-            for k in 0..num {
-                if i+k+1 >= num_cards {
-                    break;
-                }
-                card_counts[i+k+1] += 1;
+        //println!("Card wins: {}", num);
+        for k in 0..num {
+            if i+k+1 >= num_cards {
+                break;
             }
+            card_counts[i+k+1] += count;
         }
     }
 
