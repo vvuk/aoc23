df1b655666543389edb4650fd67ca4764ff579fa
Vladimir Vukicevic <vladimir@pobox.com>
2023-12-08 23:24:39 +0000

    updates

--- src/day8.rs
+++ src/day8.rs
@@ -21,74 +21,88 @@ impl Direction {
 
 #[derive(Debug, Clone)]
 struct Item {
-    code: String,
-    left: String,
-    right: String,
+    left: usize,
+    right: usize,
+    is_end: bool,
 }
 
 impl Item {
-    fn go(&self, d: Direction) -> &str {
+    fn go(&self, d: Direction) -> usize {
         match d {
-            Direction::Left => &self.left,
-            Direction::Right => &self.right,
+            Direction::Left => self.left,
+            Direction::Right => self.right,
         }
     }
 }
 
 fn main() {
     let data = include_str!("../inputs/day8.txt");
-
-    let mut items_array: Vec<Item> = Vec::new();
-    let mut items: HashMap<&str, &Item> = HashMap::new();
     let mut lines = data.lines();
     let dr = lines.next().unwrap().chars().map(|c| Direction::from_char(c)).collect_vec();
     _ = lines.next();
-    let mut path: Vec<&str> = Vec::new();
+
+    let mut name_to_item: HashMap<String, usize> = HashMap::new();
+    let mut items: Vec<Option<Item>> = Vec::new();
+
+    let mut path: Vec<usize> = Vec::new();
 
     for line in lines {
         // syntax is "AAA = (BBB, CCC)", parse into Item
         let re = Regex::new(r"(\w+) = \((\w+), (\w+)\)").unwrap();
         let caps = re.captures(line).unwrap();
-        let item = Item {
-            code: caps[1].to_string(),
-            left: caps[2].to_string(),
-            right: caps[3].to_string(),
-        };
+        let code_s = &caps[1];
+        let left_s = &caps[2];
+        let right_s = &caps[3];
 
-        items_array.push(item);
-    }
+        let code = *name_to_item.entry(code_s.to_string()).or_insert_with(|| {
+            let value = items.len();
+            items.push(None);
+            value
+        });
 
-    for item in &items_array {
-        if item.code.chars().last().unwrap() == 'A' {
-            path.push(&item.code);
+        let left = *name_to_item.entry(left_s.to_string()).or_insert_with(|| {
+            let value = items.len();
+            items.push(None);
+            value
+        });
+
+        let right = *name_to_item.entry(right_s.to_string()).or_insert_with(|| {
+            let value = items.len();
+            items.push(None);
+            value
+        });
+
+        let is_end = code_s.ends_with('Z');
+        if code_s.ends_with('A') {
+            path.push(code);
         }
 
-        items.insert(&item.code, item);
+        let item = Item { left, right, is_end };
+        items[code] = Some(item);
     }
 
     let mut result: i64 = 0;
     let mut dir_idx = 0;
     let plen = path.len();
-    'OUTER: loop {
-        if result % 100000 == 0 {
-            println!("{}: {:?}", result, path);
-        }
+    loop {
+        //if result % 100000 == 0 {
+            //println!("{}: {:?}", result, path);
+       // }
         let dir = dr[dir_idx];
 
+        let mut is_end = true;
         for i in 0..plen {
-            let item = items[&path[i]];
+            let item = items[path[i]].as_ref().unwrap();
+            is_end = is_end && item.is_end;
             path[i] = item.go(dir);
+        }
+
+        if is_end {
+            break;
         }
 
         result += 1;
         dir_idx = (dir_idx + 1) % dr.len();
-
-        for p in &path {
-            if !p.ends_with('Z') {
-                continue 'OUTER;
-            }
-        }
-        break
     }
 
     println!("result: {}", result);
